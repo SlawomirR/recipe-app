2cdb0c47eac3a749e29d2889859a442a30615d4e
SlawomirR <no.for.spam@wp.pl>
2018-08-03 18:07:44 +0200

    Fixed some names and settings to make project run.

--- src/main/java/com/example/recipe_app/RecipeSpring5Application.java
+++ src/main/java/com/example/recipe_app/RecipeSpring5Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeAppSpring5Application {
+public class RecipeSpring5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeAppSpring5Application.class, args);
+		SpringApplication.run(RecipeSpring5Application.class, args);
 	}
 }
