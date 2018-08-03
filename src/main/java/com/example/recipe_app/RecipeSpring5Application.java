0568b7a1530ed9bf2843676c2a8d9a3bc70fa948
SlawomirR <no.for.spam@wp.pl>
2018-08-03 17:12:34 +0200

    Initial commit

---
+++ src/main/java/com/example/recipe_app/RecipeSpring5Application.java
@@ -0,0 +1,12 @@
+package com.example.recipe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppSpring5Application.class, args);
+	}
+}
