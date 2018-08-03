d1d7e6d5b2fadf7afaa5aca6265f34c317ad5c60
SlawomirR <no.for.spam@wp.pl>
2018-08-03 18:30:52 +0200

    Created sample test page using Thymeleaf.

---
+++ src/main/java/com/example/recipe_app/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.recipe_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/index", "/index.html"})
+    public String getIndexPage() {
+
+        return "index";
+    }
+}
